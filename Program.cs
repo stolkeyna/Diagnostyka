8f49c7eca2c892b6484ab1b6e78ebdd1ed9fe574
MariiaLysiana <MariiaLysiana@TE-GDA-D-D21E12>
2026-09-02 11:39:59 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Diagnostyka;
+// (Diagnostyka: nazwa projektu w którym są pliki);
+
+Product p = new Product("Laptop", 1500.00f, "Electronics");
+Product p2 = new Product("Smartphone", 800.00f, "Electronics");
+Product p3 = new Product("Headphones", 200.00f, "Electronics");
+
+List<Product> products = new List<Product> ();
+products.Add(p);
+products.Add(p2);
+products.Add(p3);
+
+foreach (Product product in products)
+{
+	Console.WriteLine(product.name);
+}
---
+++ product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Diagnostyka
+{
+	class Product
+	{
+		string name;
+		float price;
+		string category;
+
+		// konstruktor inicjuje obiekt wartościami przekazanymi jako argumenty
+		public Product(string name, float price, string category)
+		{
+			this.name = name;
+			this.price = price;
+			this.category = category;
+		}
+	}
+}
